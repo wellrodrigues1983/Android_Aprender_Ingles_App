9e13df1716fc0dbea03830cde21213623ad8715e
Wellington Rodrigues <wellrodrigues@gmail.com>
2021-10-05 18:27:48 -0300

    Inserindo, manipulando e reproduzindo audio dos botões

--- app/src/main/java/com/example/aprenderingles/Fragments/NumerosFragment.java
+++ app/src/main/java/com/example/aprenderingles/Fragments/NumerosFragment.java
@@ -1,6 +1,7 @@
 package com.example.aprenderingles.Fragments;
 
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,13 +9,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.aprenderingles.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class NumerosFragment extends Fragment {
+public class NumerosFragment extends Fragment implements View.OnClickListener {
+
+    private ImageButton buttonUm, buttonDois, buttonTres,
+            buttonQuatro, buttonCinco, buttonSeis;
+
+    private MediaPlayer mediaPlayer;
 
 
     public NumerosFragment() {
@@ -25,8 +32,80 @@ public class NumerosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_numeros, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_numeros, container, false);
+
+        buttonUm = view.findViewById(R.id.buttonUm);
+        buttonDois = view.findViewById(R.id.buttonDois);
+        buttonTres = view.findViewById(R.id.buttonTres);
+        buttonQuatro = view.findViewById(R.id.buttonQuatro);
+        buttonCinco = view.findViewById(R.id.buttonCinco);
+        buttonSeis = view.findViewById(R.id.buttonSeis);
+
+        //Aplicar Evento de Click
+        buttonUm.setOnClickListener(this);
+        buttonDois.setOnClickListener(this);
+        buttonTres.setOnClickListener(this);
+        buttonQuatro.setOnClickListener(this);
+        buttonCinco.setOnClickListener(this);
+        buttonSeis.setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.buttonUm :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.one);
+                tocarSom();
+                break;
+            case R.id.buttonDois :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.two);
+                tocarSom();
+                break;
+            case R.id.buttonTres :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.three);
+                tocarSom();
+                break;
+            case R.id.buttonQuatro :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.four);
+                tocarSom();
+                break;
+            case R.id.buttonCinco :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.five);
+                tocarSom();
+                break;
+            case R.id.buttonSeis :
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.six);
+                tocarSom();
+                break;
+        }
+
+    }
+
+    public void tocarSom(){
+        if (mediaPlayer != null ){
+            mediaPlayer.start();
+
+            //Liberar os recursos após tocar o som
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    mediaPlayer.release();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
 }
