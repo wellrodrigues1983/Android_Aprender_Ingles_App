9ea019198fd080d131de407a2ad8ccb72790cc35
Wellington Rodrigues <wellrodrigues@gmail.com>
2021-10-05 14:30:23 -0300

    Criação e configuração SmartTabLayout

---
+++ app/src/main/java/com/example/aprenderingles/Fragments/BichosFragment.java
@@ -0,0 +1,32 @@
+package com.example.aprenderingles.Fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.aprenderingles.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BichosFragment extends Fragment {
+
+
+    public BichosFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bichos, container, false);
+    }
+
+}
